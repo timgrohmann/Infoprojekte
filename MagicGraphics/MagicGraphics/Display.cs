466f0e346e0b30c24cc8d5698bbeed92d83f119e
Tim Grohmann <timgrohmann@icloud.com>
2017-10-27 13:12:00 +0200

    changes

---
+++ MagicGraphics/MagicGraphics/Display.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MagicGraphics
+{
+    class Display
+    {
+        float scale;
+        Vector center;
+
+        Display(float s, Vector v)
+        {
+            scale = s;
+            center = v;
+        }
+
+        Vector toCoordinateSpace(Vector v)
+        {
+
+        }
+    }
+}
