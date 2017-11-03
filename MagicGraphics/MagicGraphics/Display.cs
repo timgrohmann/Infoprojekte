bc336000aa52f858e9f7ec364fc04480efa71fd9
Tim Grohmann <timgrohmann@icloud.com>
2017-11-03 12:02:48 +0100

    changes

--- MagicGraphics/MagicGraphics/Display.cs
+++ MagicGraphics/MagicGraphics/Display.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace MagicGraphics
 {
@@ -11,16 +13,31 @@ namespace MagicGraphics
     {
         float scale;
         Vector center;
+        PictureBox picBox;
+        
 
-        Display(float s, Vector v)
+        Display(float s, Vector v, PictureBox picBox)
         {
             scale = s;
             center = v;
+            this.picBox = picBox;
         }
 
         Vector toCoordinateSpace(Vector v)
         {
+            return Vector.Multiply(Vector.Subtract(v, center),scale);
+        }
 
+        Vector toDisplaySpace(Vector v)
+        {
+            return Vector.Subtract(Vector.Divide(v, scale), center);
+        }
+
+        void connectPoints(Vector[] inp)
+        {
+            Vector[] converted = inp.Select(i => toDisplaySpace(i)).ToArray();
+            Pen p = new Pen(Color.Black);
+            
         }
     }
 }
